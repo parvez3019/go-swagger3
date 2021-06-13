8aeb14b691999cb2ba23be2a1359081250679f90
parvez3019 <pha3019@gmail.com>
2021-06-13 18:05:23 +0530

    Refactor schema parser, extract methods for better readiblity

--- parser/schema/schema.go
+++ parser/schema/schema.go
@@ -73,50 +73,18 @@ func (p *parser) RegisterType(pkgPath, pkgName, typeName string) (string, error)
 }
 
 func (p *parser) ParseSchemaObject(pkgPath, pkgName, typeName string) (*SchemaObject, error) {
+	schemaObject, err, isBasicType := p.parseBasicTypeSchemaObject(pkgPath, pkgName, typeName)
+	if isBasicType {
+		return schemaObject, err
+	}
+
+	return p.parseCustomTypeSchemaObject(pkgPath, pkgName, typeName)
+}
+
+func (p *parser) parseCustomTypeSchemaObject(pkgPath string, pkgName string, typeName string) (*SchemaObject, error) {
 	var typeSpec *ast.TypeSpec
 	var exist bool
 	var schemaObject SchemaObject
-	var err error
-
-	// handler basic and some specific typeName
-	if strings.HasPrefix(typeName, "[]") {
-		schemaObject.Type = "array"
-		itemTypeName := typeName[2:]
-		schema, ok := p.KnownIDSchema[utils.GenSchemaObjectID(pkgName, itemTypeName, p.SchemaWithoutPkg)]
-		if ok {
-			schemaObject.Items = &SchemaObject{Ref: utils.AddSchemaRefLinkPrefix(schema.ID)}
-			return &schemaObject, nil
-		}
-		schemaObject.Items, err = p.ParseSchemaObject(pkgPath, pkgName, itemTypeName)
-		if err != nil {
-			return nil, err
-		}
-		return &schemaObject, nil
-	} else if strings.HasPrefix(typeName, "map[]") {
-		schemaObject.Type = "object"
-		itemTypeName := typeName[5:]
-		schema, ok := p.KnownIDSchema[utils.GenSchemaObjectID(pkgName, itemTypeName, p.SchemaWithoutPkg)]
-		if ok {
-			schemaObject.Items = &SchemaObject{Ref: utils.AddSchemaRefLinkPrefix(schema.ID)}
-			return &schemaObject, nil
-		}
-		schemaProperty, err := p.ParseSchemaObject(pkgPath, pkgName, itemTypeName)
-		if err != nil {
-			return nil, err
-		}
-		schemaObject.Properties = orderedmap.New()
-		schemaObject.Properties.Set("key", schemaProperty)
-		return &schemaObject, nil
-	} else if typeName == "time.Time" {
-		schemaObject.Type = "string"
-		schemaObject.Format = "date-time"
-		return &schemaObject, nil
-	} else if strings.HasPrefix(typeName, "interface{}") {
-		return &SchemaObject{Type: "object"}, nil
-	} else if utils.IsGoTypeOASType(typeName) {
-		schemaObject.Type = utils.GoTypesOASTypes[typeName]
-		return &schemaObject, nil
-	}
 
 	// handler other type
 	typeNameParts := strings.Split(typeName, ".")
@@ -221,8 +189,53 @@ func (p *parser) ParseSchemaObject(pkgPath, pkgName, typeName string) (*SchemaOb
 			propertySchema.Type = utils.GoTypesOASTypes[typeAsString]
 		}
 	}
-
 	return &schemaObject, nil
+}
+
+func (p *parser) parseBasicTypeSchemaObject(pkgPath string, pkgName string, typeName string) (*SchemaObject, error, bool) {
+	var schemaObject SchemaObject
+	var err error
+
+	// handler basic and some specific typeName
+	if strings.HasPrefix(typeName, "[]") {
+		schemaObject.Type = "array"
+		itemTypeName := typeName[2:]
+		schema, ok := p.KnownIDSchema[utils.GenSchemaObjectID(pkgName, itemTypeName, p.SchemaWithoutPkg)]
+		if ok {
+			schemaObject.Items = &SchemaObject{Ref: utils.AddSchemaRefLinkPrefix(schema.ID)}
+			return &schemaObject, nil, true
+		}
+		schemaObject.Items, err = p.ParseSchemaObject(pkgPath, pkgName, itemTypeName)
+		if err != nil {
+			return nil, err, true
+		}
+		return &schemaObject, nil, true
+	} else if strings.HasPrefix(typeName, "map[]") {
+		schemaObject.Type = "object"
+		itemTypeName := typeName[5:]
+		schema, ok := p.KnownIDSchema[utils.GenSchemaObjectID(pkgName, itemTypeName, p.SchemaWithoutPkg)]
+		if ok {
+			schemaObject.Items = &SchemaObject{Ref: utils.AddSchemaRefLinkPrefix(schema.ID)}
+			return &schemaObject, nil, true
+		}
+		schemaProperty, err := p.ParseSchemaObject(pkgPath, pkgName, itemTypeName)
+		if err != nil {
+			return nil, err, true
+		}
+		schemaObject.Properties = orderedmap.New()
+		schemaObject.Properties.Set("key", schemaProperty)
+		return &schemaObject, nil, true
+	} else if typeName == "time.Time" {
+		schemaObject.Type = "string"
+		schemaObject.Format = "date-time"
+		return &schemaObject, nil, true
+	} else if strings.HasPrefix(typeName, "interface{}") {
+		return &SchemaObject{Type: "object"}, nil, true
+	} else if utils.IsGoTypeOASType(typeName) {
+		schemaObject.Type = utils.GoTypesOASTypes[typeName]
+		return &schemaObject, nil, true
+	}
+	return nil, nil, false
 }
 
 func (p *parser) getTypeSpec(pkgName, typeName string) (*ast.TypeSpec, bool) {
