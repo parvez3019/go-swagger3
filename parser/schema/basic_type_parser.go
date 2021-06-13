ca1dc686028981e4782e9b15b8e13c635d944025
parvez3019 <pha3019@gmail.com>
2021-06-13 18:39:18 +0530

    Extract methods in basic type parser

--- parser/schema/basic_type_parser.go
+++ parser/schema/basic_type_parser.go
@@ -10,45 +10,64 @@ import (
 func (p *parser) parseBasicTypeSchemaObject(pkgPath string, pkgName string, typeName string) (*SchemaObject, error, bool) {
 	var schemaObject SchemaObject
 	var err error
-
 	// handler basic and some specific typeName
 	if strings.HasPrefix(typeName, "[]") {
-		schemaObject.Type = "array"
-		itemTypeName := typeName[2:]
-		schema, ok := p.KnownIDSchema[utils.GenSchemaObjectID(pkgName, itemTypeName, p.SchemaWithoutPkg)]
-		if ok {
-			schemaObject.Items = &SchemaObject{Ref: utils.AddSchemaRefLinkPrefix(schema.ID)}
-			return &schemaObject, nil, true
-		}
-		schemaObject.Items, err = p.ParseSchemaObject(pkgPath, pkgName, itemTypeName)
-		if err != nil {
-			return nil, err, true
-		}
-		return &schemaObject, nil, true
+		return p.parseArrayType(pkgPath, pkgName, typeName, schemaObject, err)
 	} else if strings.HasPrefix(typeName, "map[]") {
-		schemaObject.Type = "object"
-		itemTypeName := typeName[5:]
-		schema, ok := p.KnownIDSchema[utils.GenSchemaObjectID(pkgName, itemTypeName, p.SchemaWithoutPkg)]
-		if ok {
-			schemaObject.Items = &SchemaObject{Ref: utils.AddSchemaRefLinkPrefix(schema.ID)}
-			return &schemaObject, nil, true
-		}
-		schemaProperty, err := p.ParseSchemaObject(pkgPath, pkgName, itemTypeName)
-		if err != nil {
-			return nil, err, true
-		}
-		schemaObject.Properties = orderedmap.New()
-		schemaObject.Properties.Set("key", schemaProperty)
-		return &schemaObject, nil, true
+		return p.parseMapType(pkgPath, pkgName, typeName, schemaObject)
 	} else if typeName == "time.Time" {
-		schemaObject.Type = "string"
-		schemaObject.Format = "date-time"
-		return &schemaObject, nil, true
+		return p.parseTimeType(schemaObject)
 	} else if strings.HasPrefix(typeName, "interface{}") {
-		return &SchemaObject{Type: "object"}, nil, true
+		return p.parseInterfaceType()
 	} else if utils.IsGoTypeOASType(typeName) {
-		schemaObject.Type = utils.GoTypesOASTypes[typeName]
-		return &schemaObject, nil, true
+		return p.parseBasicGoType(schemaObject, typeName)
 	}
 	return nil, nil, false
+}
+
+func (p *parser) parseBasicGoType(schemaObject SchemaObject, typeName string) (*SchemaObject, error, bool) {
+	schemaObject.Type = utils.GoTypesOASTypes[typeName]
+	return &schemaObject, nil, true
+}
+
+func (p *parser) parseInterfaceType() (*SchemaObject, error, bool) {
+	return &SchemaObject{Type: "object"}, nil, true
+}
+
+func (p *parser) parseTimeType(schemaObject SchemaObject) (*SchemaObject, error, bool) {
+	schemaObject.Type = "string"
+	schemaObject.Format = "date-time"
+	return &schemaObject, nil, true
+}
+
+func (p *parser) parseArrayType(pkgPath string, pkgName string, typeName string, schemaObject SchemaObject, err error) (*SchemaObject, error, bool) {
+	schemaObject.Type = "array"
+	itemTypeName := typeName[2:]
+	schema, ok := p.KnownIDSchema[utils.GenSchemaObjectID(pkgName, itemTypeName, p.SchemaWithoutPkg)]
+	if ok {
+		schemaObject.Items = &SchemaObject{Ref: utils.AddSchemaRefLinkPrefix(schema.ID)}
+		return &schemaObject, nil, true
+	}
+	schemaObject.Items, err = p.ParseSchemaObject(pkgPath, pkgName, itemTypeName)
+	if err != nil {
+		return nil, err, true
+	}
+	return &schemaObject, nil, true
+}
+
+func (p *parser) parseMapType(pkgPath string, pkgName string, typeName string, schemaObject SchemaObject) (*SchemaObject, error, bool) {
+	schemaObject.Type = "object"
+	itemTypeName := typeName[5:]
+	schema, ok := p.KnownIDSchema[utils.GenSchemaObjectID(pkgName, itemTypeName, p.SchemaWithoutPkg)]
+	if ok {
+		schemaObject.Items = &SchemaObject{Ref: utils.AddSchemaRefLinkPrefix(schema.ID)}
+		return &schemaObject, nil, true
+	}
+	schemaProperty, err := p.ParseSchemaObject(pkgPath, pkgName, itemTypeName)
+	if err != nil {
+		return nil, err, true
+	}
+	schemaObject.Properties = orderedmap.New()
+	schemaObject.Properties.Set("key", schemaProperty)
+	return &schemaObject, nil, true
 }
